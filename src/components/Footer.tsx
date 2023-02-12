fc31154325fafe591d1a05ee16da58e685073f4b
RileyJoseph <riley.m.joseph1@gmail.com>
2023-02-12 15:20:58 -0600

    sidebar continued

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { footerList1, footerList2, footerList3 } from 'utils/constants';
+
+const List = ({ items }: { items: string[] }) => (
+  <div className="flex flex-wrap gap-2 mt-5">
+    {footerList1.map((item) => (
+      <p key={item} className="text-gray-500 text-sm hover:underline cursor-pointer">
+        {item}
+      </p>
+    ))}
+  </div>
+)
+
+const Footer = () => {
+		return (
+				<div className="mt-6 hidden xl:block">
+					 <List items={footerList1} />
+					 <List items={footerList2} />
+					 <List items={footerList3} />
+				</div>
+		)
+}
+
+export default Footer
